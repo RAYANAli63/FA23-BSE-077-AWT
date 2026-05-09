41884bf6ca9f013681b7a7028f653194272d1bcd
RAYANAli63 <rayanshah701@gmail.com>
2026-05-09 22:31:25 +0500

    Delete lecture website directory

--- lecture website/backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, register } = require('../controllers/authController');
-
-router.post('/login', login);
-router.post('/register', register); // Disable after first admin is created
-
-module.exports = router;
